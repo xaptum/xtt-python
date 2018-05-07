d00217771149ebf0536f21f1165a427361b15f0e
David R. Bild <david.bild@xaptum.com>
2018-05-07 18:00:16 -0500

    dist: learn to create manylinux1 wheels

---
+++ make/manylinux1/build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -e -x
+
+# Python 3 is needed to build ECDAA, so add it
+# to global PTH
+export PATH=$PATH:/opt/_internal/cpython-3.6.5/bin
+
+# CMake is needed for all deps, so install it
+pip3 install cmake
+
+# Preserve dist
+if [ -d dist ]; then mv dist tmpdist; fi
+
+# Compile wheels
+for PYBIN in /opt/python/*/bin; do
+    # Build the wheel
+    "${PYBIN}"/pip install -r requirements/prod.txt
+    "${PYBIN}"/python setup.py bdist_wheel
+    rm -rf .eggs
+done
+
+# Bundle external shared libraries into the wheels
+for whl in dist/*.whl; do
+    auditwheel repair "$whl" -w tmpdist/
+done
+
+# Install packages and test
+for PYBIN in /opt/python/*/bin; do
+    "${PYBIN}/pip" install xtt --no-index -f tmpdist/
+
+    "${PYBIN}"/pip install -r requirements/test.txt
+    (cd "$HOME"; "${PYBIN}/nosetests" xtt)
+done
+
+# Restore dist
+rm -rf dist && mv tmpdist dist
---
+++ make/manylinux1/build_wheels.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e -x
+
+docker run \
+       --rm \
+       -v `pwd`:/xtt-python \
+       -w /xtt-python \
+       quay.io/pypa/manylinux1_x86_64 \
+       bash -c "make/manylinux1/build.sh"
