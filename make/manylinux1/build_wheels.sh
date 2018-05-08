c902f09993241cf4439759d5eb10a2dcb2746fb2
David R. Bild <david.bild@xaptum.com>
2018-05-08 10:34:46 -0500

    build: generate wheels for i686 arch

--- make/manylinux1/build_wheels.sh
+++ make/manylinux1/build_wheels.sh
@@ -7,3 +7,10 @@ docker run \
        -w /xtt-python \
        quay.io/pypa/manylinux1_x86_64 \
        bash -c "make/manylinux1/build.sh"
+
+docker run \
+       --rm \
+       -v `pwd`:/xtt-python \
+       -w /xtt-python \
+       quay.io/pypa/manylinux1_i686 \
+       bash -c "make/manylinux1/build.sh"
