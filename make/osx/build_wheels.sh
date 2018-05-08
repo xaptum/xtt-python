3a856710041dbbdb4941140eae14e715967d236a
David R. Bild <david.bild@xaptum.com>
2018-05-08 15:17:17 -0500

    travis: add builds on OSX

---
+++ make/osx/build_wheels.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+set +x
+
+for PYVERSION in 2.7 3.4 3.5 3.6; do
+    virtualenv -p /Library/Frameworks/Python.framework/Versions/${PYVERSION}/bin/python${PYVERSION} venv_${PYVERSION}
+    . ./venv_${PYVERSION}/bin/activate
+
+    pip install -r requirements/setup.txt
+    python setup.py bdist_wheel
+
+    pip uninstall -y xtt || true
+    pip install xtt --no-index -f dist
+
+    pip install -r requirements/test.txt
+    python setup.py test
+
+    deactivate
+done
