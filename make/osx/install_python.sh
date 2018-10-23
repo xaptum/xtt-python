511e020bce6f860e137a0f0c926e01d1e54be1d9
David R. Bild <david.bild@xaptum.com>
2018-10-23 14:02:03 -0500

    build: add Python 3.7 to OSX builds

--- make/osx/install_python.sh
+++ make/osx/install_python.sh
@@ -7,6 +7,7 @@ URLS=(
     'https://www.python.org/ftp/python/3.4.4/python-3.4.4-macosx10.6.pkg'
     'https://www.python.org/ftp/python/3.5.4/python-3.5.4-macosx10.6.pkg'
     'https://www.python.org/ftp/python/3.6.5/python-3.6.5-macosx10.9.pkg'
+    'https://www.python.org/ftp/python/3.7.0/python-3.7.0-macosx10.9.pkg'
 )
 
 pip install virtualenv
